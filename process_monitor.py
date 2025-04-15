6d34c41e36865a1a458a9ce5f063ccf28fd8496b
Gurleen Kaur Thind <150411848+gurleenkthind@users.noreply.github.com>
2025-04-15 09:23:12 +0530

    Update process_monitor.py

--- process_monitor.py
+++ process_monitor.py
@@ -1,7 +1,7 @@
 import psutil
 
 def get_system_processes():
-    """Fetch running system processes."""
+    """Fetch running system processes"""
     processes = []
     for proc in psutil.process_iter(attrs=['pid', 'name']):
         processes.append((proc.info['pid'], proc.info['name']))
